f1cfa2a733b332969e6765165d1d09dcd2074a31
Pukhliak <pukhliak@gmail.com>
2022-12-16 19:43:03 +0300

    change styles for dark theme, add switcher

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// bottom to top
 document.addEventListener("DOMContentLoaded", () => {
   let to_top_btn = document.querySelector(".to-up");
 
@@ -9,3 +10,23 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 });
+
+// theme-switcher
+
+const toggleSwitch = document.querySelector(
+  '.theme-switch-wrapper input[type="checkbox"]'
+);
+
+function switchTheme(e) {
+  let lightThemeText = document.getElementById("light-theme-text");
+  let darkThemeText = document.getElementById("dark-theme-text");
+  darkThemeText.classList.toggle("disabled");
+  lightThemeText.classList.toggle("disabled");
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "light");
+  } else {
+    document.documentElement.setAttribute("data-theme", "dark");
+  }
+}
+
+toggleSwitch.addEventListener("change", switchTheme, false);
